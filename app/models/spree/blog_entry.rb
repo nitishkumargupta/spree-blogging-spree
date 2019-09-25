0aa96aef84bdd7ccce36cc9b9532b613afb95215
Nitish <ng0883@yahoo.in>
2019-09-25 21:35:28 +0530

    blog entry image replaced with active storage

--- app/models/spree/blog_entry.rb
+++ app/models/spree/blog_entry.rb
@@ -17,9 +17,8 @@ class Spree::BlogEntry < ActiveRecord::Base
     belongs_to :author
   end
 
-  has_one :blog_entry_image, :as => :viewable, :dependent => :destroy, :class_name => 'Spree::BlogEntryImage'
-  accepts_nested_attributes_for :blog_entry_image, :reject_if => :all_blank
-
+  has_one_attached :avatar, :dependent => :destroy
+  
   def entry_summary(chars=200)
     if summary.blank?
       "#{body[0...chars]}..."
